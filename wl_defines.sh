36bd017f3874fc82751e83ab5f0f7c5cf50d00f9
Bryce Harrington <b.harrington@samsung.com>
2014-05-30 22:54:18 -0700

    Add flag for including xwayland stuff

--- wl_defines.sh
+++ wl_defines.sh
@@ -1,9 +1,11 @@
+INCLUDE_XWAYLAND=1
 WLROOT=$HOME/Wayland
 WLD=$WLROOT/install   # change this to another location if you prefer
+
 LD_LIBRARY_PATH=$WLD/lib
 PKG_CONFIG_PATH=$WLD/lib/pkgconfig/:$WLD/share/pkgconfig/
 ACLOCAL_PATH="$WLD/share/aclocal"
 ACLOCAL="aclocal -I $ACLOCAL_PATH"
 
-export WLD LD_LIBRARY_PATH PKG_CONFIG_PATH ACLOCAL ACLOCAL_PATH
+export WLROOT WLD LD_LIBRARY_PATH PKG_CONFIG_PATH ACLOCAL ACLOCAL_PATH
 
