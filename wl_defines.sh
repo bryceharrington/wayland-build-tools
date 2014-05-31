aa833311c6e7985e7c8c13e1da2f80e6bd816c5a
Bryce Harrington <b.harrington@samsung.com>
2014-05-30 23:16:17 -0700

    It may be necessary to build libxtrans from xserver on 32-bit systems.

--- wl_defines.sh
+++ wl_defines.sh
@@ -2,6 +2,12 @@ INCLUDE_XWAYLAND=1
 WLROOT=$HOME/Wayland
 WLD=$WLROOT/install   # change this to another location if you prefer
 
+if [ "$(uname -i)" = "i386" ]; then
+	WL_BITS=32
+else
+	WL_BITS=64
+fi
+
 LD_LIBRARY_PATH=$WLD/lib
 PKG_CONFIG_PATH=$WLD/lib/pkgconfig/:$WLD/share/pkgconfig/
 ACLOCAL_PATH="$WLD/share/aclocal"
