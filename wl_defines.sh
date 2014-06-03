a4fef9fd1e3a9d434c98b85e398e47e285aaee6d
Bryce Harrington <b.harrington@samsung.com>
2014-06-03 00:59:39 -0700

    Bring tools up to date with XWayland current git dependencies

--- wl_defines.sh
+++ wl_defines.sh
@@ -1,17 +1,17 @@
-INCLUDE_XWAYLAND=1
-WLROOT=$HOME/Wayland
-WLD=$WLROOT/install   # change this to another location if you prefer
+export INCLUDE_XWAYLAND=1
+export WLROOT=$HOME/Wayland
+export WLD=$WLROOT/install   # change this to another location if you prefer
 
 if [ "$(uname -i)" = "i386" ]; then
-	WL_BITS=32
+	export WL_BITS=32
 else
-	WL_BITS=64
+	export WL_BITS=64
 fi
 
-LD_LIBRARY_PATH=$WLD/lib
-PKG_CONFIG_PATH=$WLD/lib/pkgconfig/:$WLD/share/pkgconfig/
-ACLOCAL_PATH="$WLD/share/aclocal"
-ACLOCAL="aclocal -I $ACLOCAL_PATH"
+export LD_LIBRARY_PATH=$WLD/lib
+export PKG_CONFIG_PATH=$WLD/lib/pkgconfig/:$WLD/share/pkgconfig/
+export PATH=$WLD/bin:$PATH
+export ACLOCAL_PATH="$WLD/share/aclocal"
+export ACLOCAL="aclocal -I $ACLOCAL_PATH"
 
-export WLROOT WLD LD_LIBRARY_PATH PKG_CONFIG_PATH ACLOCAL ACLOCAL_PATH
 
