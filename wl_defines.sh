07f8ed3663a3ae23fd11ae108ffb1644f21b4bf8
Bryce Harrington <bryce@osg.samsung.com>
2015-12-23 11:22:35 -0800

    Fix weston's distcheck by specifying path to the Xwayland binary

--- wl_defines.sh
+++ wl_defines.sh
@@ -8,10 +8,12 @@ else
 	export WL_BITS=64
 fi
 
+XWAYLAND=${WLROOT}/install/bin/Xwayland
+export DISTCHECK_CONFIGURE_FLAGS="--with-xserver-path=$XWAYLAND"
+
 export LD_LIBRARY_PATH=$WLD/lib
 export PKG_CONFIG_PATH=$WLD/lib/pkgconfig/:$WLD/share/pkgconfig/
 export PATH=$WLD/bin:$PATH
 export ACLOCAL_PATH="$WLD/share/aclocal"
 export ACLOCAL="aclocal -I $ACLOCAL_PATH"
-
 
